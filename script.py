f6fd750915a940e02aa2251effe0f35ddb372b87
revsi <therevolutionizt@gmail.com>
2014-11-14 23:02:18 +0530

    using shallow_parser_command command

--- script.py
+++ script.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys, getopt, os
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print 'Input file is : ', inputfile
-   print 'Output file is : ', outputfile
+    inputfile = ''
+    outputdirectory = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputdirectory>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputdirectory>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputdirectory = arg
+
+    # abs_in is the absolute path of input file
+    # abs_out is the absolute path of output folder
+    abs_out = os.path.abspath(os.path.dirname(outputdirectory))
+    abs_in = os.path.realpath(os.path.join(inputfile))
+    shallow_parser(abs_in, abs_out)
+
+def shallow_parser(inp,out):
+    shallow_command = ''
+    shallow_command = 'shallow_parser_hin ' + str(inp)
+    print shallow_command
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
