58e3426f80ce2e91f8e622433135711f2d4c2321
Rajat <therevolutionizt@gmail.com>
2014-12-01 02:45:41 +0530

    sorting of frames

--- pruning.py
+++ pruning.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 import ast
-def sorting(inp, out):
-    A=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    outfile = out +'/pruned'
+def sorting(inp,out):
     f_read = open(inp, "r")
-    for line in f_read:
-        line = ast.literal_eval(line)
-        print str(line) + " : "+str(len(line))
+    output = out + "out"
+    lines = [ast.literal_eval(line) for line in f_read if line.strip()]
+    f_read.close()
+    lines.sort(key=lambda line: len(line))
+    f_write = open(output,'w')
+    for line in lines:
+        f_write.write(str(line)+"\n")
 
 
 
-    #contents = f.readlines()
-    #contents.insert(index, value)
-    #f_write = open(out, "w")
-    #contents = "".join(contents)
-    #f_write.write(contents)
-    #f_read.close()
-    #f_write.close()
+#f = open("path_to_file", "r")
+#contents = f.readlines()
+#f.close()
 
-sorting("/home/rajat/academics/CL1/project/corpus/test/verbframes", "/home/rajat/academics/CL1/project/output/")
+#contents.insert(index, value)
+
+#f = open("path_to_file", "w")
+#contents = "".join(contents)
+#f.write(contents)
+#f.close()
+
+sorting("/home/rajat/academics/CL1/project/corpus/test/verbframes","/home/rajat/academics/CL1/project/corpus/test/")
